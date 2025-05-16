99f448671af22a338a547fe2a8275dc5485246ba
KenjiroSato <159330845+KenjiroSato@users.noreply.github.com>
2025-05-15 19:31:33 -0500

    se crea la clase nodo
    se completa la clase nodo para el taller

---
+++ Arboles/src/Nodo.java
@@ -0,0 +1,14 @@
+public class Nodo {
+    int valor;
+    Nodo izquierdo;
+    Nodo derecho;
+    
+    public Nodo(int valor){
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    
+
+}
